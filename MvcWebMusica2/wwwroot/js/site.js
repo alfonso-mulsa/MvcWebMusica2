57b8b36307a03b5abf4a0cb90800d7d6a1076327
Alfonso Mulsa <amulsa@gmail.com>
2024-05-23 22:38:50 +0200

    Cambiar Edit, Details y Delete por iconos

--- MvcWebMusica2/wwwroot/js/site.js
+++ MvcWebMusica2/wwwroot/js/site.js
@@ -1,12 +1,26 @@
 ﻿$(function () {
-    $("#tabla-indice").fancyTable({
+    $(".tabla-indice").fancyTable({
         sortColumn: 0, // column number for initial sorting
-        sortOrder: 'descending', // 'desc', 'descending', 'asc', 'ascending', -1 (descending) and 1 (ascending)
+        sortOrder: 'ascending', // 'desc', 'descending', 'asc', 'ascending', -1 (descending) and 1 (ascending)
         sortable: true,
         pagination: true, // default: false
+        paginationClass: "btn btn-primary",
+        paginationClassActive: "active",
+        pagClosest: 3,
+        perPage: 15,
         searchable: true,
-        globalSearch: true,
+        globalSearch: true, // use global search for all columns
+        //globalSearchExcludeColumns: [2, 5]// exclude column 2 & 5
+        //matchCase: true, // use case sensitive search
+        //exactMatch: true, // use exact match
         inputStyle: "",
-        inputPlaceholder: "Buscar..."
+        inputPlaceholder: "Buscar...",
+        //sortFunction: function (a, b, o) {
+        //    if (o.sortAs[o.sortColumn] == 'numeric') {
+        //        return ((o.sortOrder > 0) ? parseFloat(a) - parseFloat(b) : parseFloat(b) - parseFloat(a));
+        //    } else {
+        //        return ((a < b) ? -o.sortOrder : (a > b) ? o.sortOrder : 0);
+        //    }
+        //},
     });
 });
