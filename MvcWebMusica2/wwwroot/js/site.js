c6e0b353b91ba193da768babeba34fe7338e0abe
Alfonso Mulsa <amulsa@gmail.com>
2024-05-24 23:16:41 +0200

    Mejoras en el diseño de fondos y de las tablas.

--- MvcWebMusica2/wwwroot/js/site.js
+++ MvcWebMusica2/wwwroot/js/site.js
@@ -1,26 +1,38 @@
 ﻿$(function () {
-    $(".tabla-indice").fancyTable({
-        sortColumn: 0, // column number for initial sorting
-        sortOrder: 'ascending', // 'desc', 'descending', 'asc', 'ascending', -1 (descending) and 1 (ascending)
-        sortable: true,
-        pagination: true, // default: false
-        paginationClass: "btn btn-primary",
-        paginationClassActive: "active",
-        pagClosest: 3,
-        perPage: 15,
-        searchable: true,
-        globalSearch: true, // use global search for all columns
-        //globalSearchExcludeColumns: [2, 5]// exclude column 2 & 5
-        //matchCase: true, // use case sensitive search
-        //exactMatch: true, // use exact match
-        inputStyle: "",
-        inputPlaceholder: "Buscar...",
-        //sortFunction: function (a, b, o) {
-        //    if (o.sortAs[o.sortColumn] == 'numeric') {
-        //        return ((o.sortOrder > 0) ? parseFloat(a) - parseFloat(b) : parseFloat(b) - parseFloat(a));
-        //    } else {
-        //        return ((a < b) ? -o.sortOrder : (a > b) ? o.sortOrder : 0);
-        //    }
-        //},
+    // ***************** Crear y definir propiedades de tabla '.tabla-indice' *******************
+    DataTable.datetime('DD/MM/YYYY');
+    $('.tabla-indice').DataTable({
+        search: {
+            return: true
+        },
+        language: {
+            lengthMenu: 'Mostrar _MENU_ entradas por página',
+            emptyTable: 'No hay datos disponibles',
+            info: 'Mostrando _START_ a _END_ de _TOTAL_ entradas',
+            infoEmpty: 'Mostrando 0 a 0 de 0 entradas',
+            infoFiltered: '(filtradas de _MAX_ entradas totales)',
+            search: '',
+            searchPlaceholder: 'Filtrar entradas por...',
+            zeroRecords: 'No se encontraron entradas coincidentes.',
+        },
+        layout: {
+            topStart: 'search',
+            topEnd: 'pageLength',
+            bottomStart: 'paging',
+            bottomEnd: 'info',
+        },
+        colReorder: true,
     });
+
+    // ***************** Carrusel de fondo *******************
+    const imagenes = ['./img/fondos/fondo1-byn.jpg', './img/fondos/fondo2-byn.jpg', './img/fondos/fondo3-byn.jpg', './img/fondos/fondo4-byn.jpg'];
+    var numFondo = 1;
+    setInterval(cambiarFondo, 5000);
+
+    function cambiarFondo() {
+        $("body").css("background-image", "url('" + imagenes[numFondo] + "')");
+        numFondo += 1;
+        if (numFondo === imagenes.length) { numFondo = 0 };
+    }
+
 });
