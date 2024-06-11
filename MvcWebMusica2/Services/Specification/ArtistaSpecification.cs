e9239cd507bec0be6a60323b3828ca5210fb89e2
AdrianCruzG <adrian.cruz.gomez1804@gmail.com>
2024-06-11 14:43:49 +0200

    creado servicios y GruposViewcomponent

---
+++ MvcWebMusica2/Services/Specification/ArtistaSpecification.cs
@@ -0,0 +1,12 @@
+﻿using MvcWebMusica2.Models;
+
+namespace MvcWebMusica2.Services.Specification
+{
+    public class ArtistaSpecification (int ArtistaId): IGrupoSpecification
+    {
+        public bool IsValid(Grupos element)
+        {
+            return element.Id == ArtistaId;
+        }
+    }
+}
