013d990afb92a53f14d913a996dcdab60ffdc79d
maria-programa <mlozanoovejero314@gmail.com>
2024-06-12 11:29:00 +0200

    Filtradas las giras por los conciertos que tiene (puesto con la ciudad y la fecha en la que es el concierto)

---
+++ MvcWebMusica2/Services/Specification/GiraSpecification.cs
@@ -0,0 +1,12 @@
+﻿using MvcWebMusica2.Models;
+
+namespace MvcWebMusica2.Services.Specification
+{
+    public class GiraSpecification(int GiraId) : IConciertoSpecification
+    {
+        public bool IsValid(Conciertos element)
+        {
+            return element.GirasId == GiraId;
+        }
+    }
+}
