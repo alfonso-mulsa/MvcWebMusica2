96e8537264110dab9669ca45bf2f73ca325128c2
Andrés Aragón <aragonandresjulian@gmail.com>
2024-06-12 12:08:41 +0200

    Solucionado el fallo en el ViewComponent.

--- MvcWebMusica2/Views/Shared/Components/RepresentantesViewComponent.cs
+++ MvcWebMusica2/Views/Shared/Components/RepresentantesViewComponent.cs
@@ -1,25 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using MvcWebMusica2.Models;
 using MvcWebMusica2.Services.Repositorio;
 using MvcWebMusica2.Services.Specification;
-using System.Linq;
+using MvcWebMusica2.Models;
 
 namespace MvcWebMusica2.Views.Shared.Components
 {
     public class RepresentantesViewComponent (IGenericRepositorio<Grupos> coleccion) : ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync(int representanteId) //IGrupoSpecification especificacion
+        public async Task<IViewComponentResult> InvokeAsync(IGrupoSpecification especificacion)
         {
-            //IEnumerable<Grupos> coleccionInicial = await coleccion.DameTodos();
-            //if (especificacion is not null)
-            //{
-            //    coleccionInicial = coleccion.Where(especificacion.IsValid);
-            //}
-
-            //return View();
-
             var items = await coleccion.DameTodos();
-            IGrupoSpecification especificacion = new RepresentanteSpecification(representanteId);
             var itemsFiltrados = items.Where(especificacion.IsValid);
             return View(itemsFiltrados);
         }
