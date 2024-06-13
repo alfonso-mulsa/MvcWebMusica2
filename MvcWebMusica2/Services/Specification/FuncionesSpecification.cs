f2ee3a6a0bffc98cf21951a465893cbec78d7962
Mercedes Librero <mercedeslibrero@gmail.com>
2024-06-13 14:48:02 +0200

    De nuevo Artistas y Funciones

---
+++ MvcWebMusica2/Services/Specification/FuncionesSpecification.cs
@@ -0,0 +1,14 @@
+﻿
+using MvcWebMusica2.Models;
+
+
+namespace MvcWebMusica2.Services.Specification
+{
+    public class FuncionesSpecification (int ArtistaId): IFuncionSpecification
+    {
+        public bool IsValid(FuncionesArtistas element)
+        {
+            return element.ArtistasId == ArtistaId;
+        }
+    }
+}
---
+++ MvcWebMusica2/Views/Shared/Components/ArtistasFunciones/ArtistasFuncionesViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using MvcWebMusica2.Models;
+using MvcWebMusica2.Services.Repositorio;
+using MvcWebMusica2.Services.Specification;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace MvcWebMusica2.Views.Shared.Components
+{
+    public class ArtistasFuncionesViewComponent (IGenericRepositorio<FuncionesArtistas> coleccion): ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(IFuncionSpecification especificacion)
+        {
+            var items = await coleccion.DameTodos();
+            var itemsFiltrados = items.Where(especificacion.IsValid);
+            return View(itemsFiltrados);
+        }
+    }
+}
