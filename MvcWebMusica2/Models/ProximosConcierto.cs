40faa0319e9b6730a686f869552756f8fe85249a
Mercedes Librero <mercedeslibrero@gmail.com>
2024-06-20 12:29:36 +0200

    Prueba Proximos conciertos

---
+++ MvcWebMusica2/Models/ProximosConcierto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MvcWebMusica2.Models;
+
+public  class ProximosConcierto
+{
+    public int Id { get; set; }
+
+    public DateOnly? Fecha { get; set; }
+
+    public string? Gira { get; set; }
+
+    public string? Ciudad { get; set; }
+
+    public string? Grupo { get; set; }
+}
